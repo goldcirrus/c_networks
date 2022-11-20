49de4173cf12532efe060d241398b2bfa0d92ed6
goldcirrus <53105370+goldcirrus@users.noreply.github.com>
2022-11-19 22:14:09 -0800

    Add files via upload

--- tcp_echo_client.c
+++ tcp_echo_client.c
@@ -1,4 +1,4 @@
-// TCP echo client program
+// TCP echo client program; Liang Zhong; T00224182
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,56 +8,91 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define BUFFSIZE 512	/* buffer size */
+
 int main (int argc, char* argv[ ])		// Three arguments to be checked later
 {
-	struct sockaddr_in servAddr;		// Server socket address data structure
-	char *servIP = argv[1];				// Server IP address from command line
-	int servPort = atoi(argv[2]);		// Server port number from command line
-	char *message = argv[3];			// Message specified on the command line
+        int socket_fd;                               //socket descriptor(reference to client socket)
+	
+        struct sockaddr_in servAddr;    /*declare server's socket address variable*/
+                               /*no need to explicitly define client socket address variable because OS knows it*/
+        char buffer[BUFFSIZE];        //buffer holding the received message echoed back from server
+        char* ptr=buffer;                    //pointer to move along the buffer for next byte to be received
+        int n=0;                                //number of bytes(characters) in each recv() call
+        int len=0;                             //length of message to be echoed in buffer     
+        int maxLen=BUFFSIZE;             //maximum number of bytes in buffer                              
+        
+
+        char *servIP;				// Server IP address from command line
+	int servPort;		// Server port number from command line
+	char *message;			// Message specified on the command line
+        
+        
 
 	// Check for correct number of command line arguments
 	if(argc != 4) {                                               
 		printf("tcp-echo-client [IP address] [Port] [Message]\n");                             
 		exit (1);
 	}                                                
+        
+        servIP=argv[1];
+        servPort=atoi(argv[2]);
+        message=argv[3];
 
-	// Populate socket address for the server
-	memset (&servAddr, 0, sizeof(servAddr));		// Initialize data structure
-	servAddr.sin_family = AF_INET; 					// This is an IPv4 address
-	servAddr.sin_addr.s_addr = inet_addr(servIP); 	// Server IP address
-	servAddr.sin_port = servPort;					// Server port number
+	// Populate/define the remote(server) socket address for the variable 'servAddr'
+	memset (&servAddr, 0, sizeof(servAddr));		// Initialize server's socket address variable's data structure(assign memory)
+	servAddr.sin_family = AF_INET; 				// server's socket address variable get its faimly struct field: This is an IPv4 address
+	servAddr.sin_addr.s_addr = inet_addr(servIP); 	        // Server's socket address variable get its IP address by command line argument input
+	servAddr.sin_port = htons(servPort);	         	// server's socket address variable get its port value from command line argument input: Server port number integer converted to binary
 	
-	// Create a TCP socket stream
-	int sock;
-	if ((sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+
+/* Create a TCP client stream socket: OS knows its socket address fields value, so no need to explictly define its socket address. */
+	
+	if ((socket_fd = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
 		printf("Error: socket creation failed!\n");
 		exit (1);
 	}
 	else
-        printf("Socket successfully created..\n"); 
+        printf("Tell: A client stream Socket is successfully created..\n"); 
 
-	// Connect  to the server
-	if ((connect (sock, (struct sockaddr*)&servAddr, sizeof(servAddr))) == -1) {
+	//Establish connection: Connect the client socket to the server socket address defined above
+	if ((connect (socket_fd, (struct sockaddr*)&servAddr, sizeof(servAddr))) == -1) {
 		printf("Error: connection to the server failed!\n");
 		exit (1);
 	}
-	 else
-        printf("Connected to the server..\n"); 
+	else
+               printf("Tell: Connect() establishment to the server is successful.\n"); 
 	
 	// Students to complete this section
 
-	// Send data to the server...
+	// Send data(message read from the command line argument argv[3] to the server...
+        send(socket_fd, message, strlen(message),0);        //send message data to client socket
+        printf("Student says: message sent out to the server Done successfully. ");
 
-	// Receive data back from the server..
-		
-	//	Loop while receiving data... 
-	//		print data...
-	// 	end-while loop  
+       // Receive data back from the server..
+       //while Loop goes forever until the server stop sending data segment to this client.  
+        while((n=recv(socket_fd, buffer, maxLen,0))>0)
+        {
+              ptr+=n;               //move pointer to next position to receive next string segment
+              maxLen-=n;            //decrement maximum capacity in buffer
+              len+=n;              //increate received number of bytes
+              
+              buffer[len]='\0';
+              printf("echo back is %s \n", buffer);              
+        }
 
+        //add '\0' end of string character in the end of string in buffer
+      //  buffer[len]='\0';
+        
+	//print data...
+        printf("\nSuccefully get the echo back from server.");
+	printf("\nThe echoed back message is  %s.\n", buffer);
+        fputs(buffer,stdout); 
+    
 	// Close socket
-	close (sock);
+	close (socket_fd);
 	
 	// Stop program
-	exit (0);
+	return 0;
 	
 } // End main
--- tcp_echo_server.c
+++ tcp_echo_server.c
@@ -8,46 +8,98 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define	BUFLEN	512											// Maximum length of buffer
-#define PORT	9988										// Fixed server port number
+#define	BUFLEN	512			// Maximum length of buffer
+#define PORT	8899			// Fixed server port number
 
 int main (void)
 {
-	struct sockaddr_in server_address;						// Data structure for server address
-	struct sockaddr_in client_address;						// Data structure for client address
-	int client_address_len = 0;
+   /*declare and define listen socket(connect establish) and socket(data transmit). */
+	struct sockaddr_in server_address;    // declare a server(local) address variable of socket address struct
+	struct sockaddr_in client_address;   // declare a client(remote) address variable of socket address struct
+	int client_address_len = 0;           //length of client address(need to be explicitly defined in function)
+        int listen_socket;                          //listen socket descriptor(connection establishment phase)
+        int s_socket;                          //socket descriptor(reference for data transmit)
+        
+    /*declare buffer for server. */
+        char buffer[BUFLEN];                     //declare a 'buffer' to hold received characters for server
+        char* ptr=buffer;                     //a pointer points to the next bytes(char) to receive in 'buffer'
+        int len=0;                         //number of bytes(chars) to receive(consume) or send(produce) in buffer
+        int maxLen=BUFLEN;            //Maximum number of bytes(characters) allowed in 'buffer'
+        int n=0;                               //number of bytes(chars) received for each recv() call
+        
+   /*define how many client's requests can be in the waiting queue. */
+        int waitSize=16;                       //size of waiting clients
 		
 	
-	// Populate socket address for the server
-	memset (&server_address, 0, sizeof (server_address));	// Initialize server address data structure
-	server_address.sin_family = AF_INET; 					// Populate family field - IPV4 protocol
-	server_address.sin_port = PORT;							// Set port number
-	server_address.sin_addr.s_addr = INADDR_ANY;			// Set IP address to IPv4 value for loacalhost
+/* 1.Populate/Create server(local) sockets address for both listen_socket and socket */
+	
+        memset (&server_address, 0, sizeof (server_address));	// Initialize:assign memory to the server address
+	server_address.sin_family = AF_INET; 		       // Populate family field - IPV4 protocol
+	server_address.sin_port = htons(PORT);	            // Set port number;htons() converts 9988 to binary
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY);	    // Set IP address to IPv4 for loacalhost(default any)
 	 	
-	// Create a TCP socket; returns -1 on failure
-	int listen_sock;
-	if ((listen_sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+/*2.Create a TCP server's listen socket; returns -1 on failure */
+	if ( (listen_socket = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP) ) == -1)
+        {
 		printf("Error: Listen socket failed!\n");
 		exit (1);
 	}
 
-	// Bind the socket to the server address; returns -1 on failure
-	if ((bind(listen_sock, (struct sockaddr *)&server_address, sizeof (server_address))) == -1) {
+/*3.Bind the server's listen socket to the server's socket address; returns -1 on failure */
+	if ((bind(listen_socket, (struct sockaddr *)&server_address, sizeof (server_address))) == -1)
+        {
 		printf("Error: binding failed!\n");
 		exit (1);
 	}
 	
-	printf("Server not fully implemented...\n");
-	// Students to complete this section
+	printf("Server is listening, waiting to connect. we will continue ...\n");
 	
-	// Listen for connections...
+//student's cods
+/*4.Start to listen to connection requests from clients by calling listen() system call. */
+        if(listen(listen_socket,waitSize) == -1) 
+        {
+               printf("Error:listening failed!");
+               exit(1);
+        }
 	
-	// Accept connections from client...
-	
-	// Receive and format the data...
-	
-	// Echo data back to the client...
+/* Forever in while loop to listen and accept connections request from client...*/
+        while(1)    
+        {
+               /*accept a connection from a client socket by accept() system call. */
+              if((s_socket = accept(listen_socket,(struct sockaddr*)&client_address,(socklen_t*)&client_address_len))==-1)
+               {
+                      printf("Error: accepting failed! Can't open a socket to accept data transmit.");
+                      exit(1);
+               }
+
+               printf("Tell: listen_socket accepts a client request return a socket for data transmit. ");
+               printf("client connected with client's ip address at %s\n",inet_ntoa(client_address.sin_addr));
+              
+
+              /*keep receiving as long as the current client keeps the connection */
+               while((n=recv(s_socket,ptr,maxLen,0))>0)
+               {
+                      printf("Tell: received message segment from a client is %s. \n",buffer);
+                      ptr+=n;    //move pointer to next postion for next bytes to be received along buffer
+                      maxLen-=n;      //decrement maximum bytes to be received 
+                      len+=n;         //increment received bytes 
+                      
+                      send(s_socket,buffer,len,0);
+
+               }
+              
+               /*the code below won't be executed since as long as the client is connected to the server, it is trapped in the while loop forever. When the connection is forced break. the send() function lost the s_socket to send to.  */
+               printf("butffer content to be sent out is %s:\n", buffer);
+               /*after receiving finishes, send back(echo) all bytes(characters) received in buffer to client*/
+               send(s_socket,buffer,len,0);
+
+//don't close listen_socket in endless while loop because server needs to listen all the time for new clients.
+//close(listen_socket) here will cause server terminate listening when the first client finish its turn
+               close(s_socket);    //close data transmit socket for the current cient finishes.      
+         }     	
+
+
 		
-	close (listen_sock);									// Cloese descriptor referencing server socket
-	
-} // End main
+         close(listen_socket);				// Cloese descriptor referencing server socket
+	 return 0;
+}                                              // End main
