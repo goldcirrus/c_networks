47cf1923271e9d1c197f17bc63eb7e3c38fd862f
goldcirrus <53105370+goldcirrus@users.noreply.github.com>
2022-11-19 22:21:54 -0800

    Add files via upload

---
+++ part4_source_code_files/fta_client.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define	MAXLINE	128	// maximum bytes to receive and send at once
+
+// External functions
+extern int swap_open(unsigned int addr, unsigned short port);
+extern int swap_write(int sd, unsigned char buf[], int length);
+extern void swap_close(int sd);
+
+// These arguments must be passed to the program
+// argv[1]: IP address of server, e.g., "127.0.0.1"
+// argv[2]: port number of sever, e.g., "8899"
+// argv[3]: input file name(file to be transfered to server)
+int main(int argc, char *argv[])
+{
+	unsigned short server_port;
+	unsigned int server_address;
+	unsigned char buf[MAXLINE];
+	int sd, n, in;
+
+	if (argc < 4) {
+		fprintf(stderr, "Usage: %s address port input-filename\n", argv[0]);
+		exit(1);
+	}
+
+	// set values for server address and port number
+	server_address = inet_addr(argv[1]);	// server address
+	server_port = htons(atoi(argv[2]));		// port number
+
+	/*connect to the swap server: pass in server IP and server port by command line argument
+	 *create a client socket, send one character(0xff) to server. */
+
+	sd = swap_open(server_address, server_port);
+	if (sd < 0) {
+		fprintf(stderr, "swap_open error\n");
+		exit(0);
+	}
+
+	//Students to code the following:
+
+	//open input file for reading
+
+	//send input file name o the server
+
+	// loop
+		//if there is data in the file, send it to the server
+	// end-loop
+
+	// close the connection to the swap server
+	swap_close(sd);
+
+	//close connection to input file
+	close(in);
+}
---
+++ part4_source_code_files/fta_server.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define	MAXLINE	128	// maximum bytes to receive and send at once
+
+// External functions
+extern int swap_wait(unsigned short port);
+extern int swap_read(int sd, unsigned char buf[]);
+extern void swap_close(int sd);
+
+// pass in:argv[1]: port number, e.g., "8899"; only need to pass in server port number
+int main(int argc, char *argv[])
+{
+	unsigned short server_port;
+	unsigned int server_address;
+	unsigned char message[MAXLINE];
+	int sd, n, out;     //out is output file's file descriptor
+
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s port\n", argv[0]);
+		exit(1);
+	}
+
+	// set values for server address(no need here) and port number(by command line argument)
+	server_port = htons(atoi(argv[1]));
+
+	// connect to the port allocated to the swap server
+	//swap_wait(sever_port_number) in swap_server.c
+	//sd is a session ID returned by swap_wait();
+	//under this sd# sever is waiting to connect with a client at a sever_port#
+	sd = swap_wait(server_port);
+	if (sd < 0) {
+		fprintf(stderr, "swap_open error\n");
+		exit(0);
+	}
+
+	//Students to code the following:
+
+	//read in the first message as the new name of the file
+
+	//open output file for writing
+
+	//begin-loop
+		//get the file data from the client until end-of-file
+	//end-loop
+
+
+
+	// close the connection to the swap server
+	swap_close(sd);
+
+	//close connection to output file; out is output file's file descriptor
+	close(out);
+}
